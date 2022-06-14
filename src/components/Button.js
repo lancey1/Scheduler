8fa21ab7d1e65a264c45fc42a82832e39c76c2c5
lancey1 <lanceyu619@gmail.com>
2022-06-14 19:30:12 -0400

    inital commit, add render of buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
+
 export default function Button(props) {
-   return <></>;
-}
+   console.log(props)
+   let btnClass = classNames("button",{'button--confirm': props.confirm},{'button--danger':props.danger})
+   
+   // let buttonClass = "button";
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // } 
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   //  }
+   // if (props.disabled) {
+   //    return <button disabled className={buttonClass}> {props.children} </button>;
+   // }
+
+   return <button onClick={props.onClick} className={btnClass} disabled = {props.disabled}> {props.children} </button>;
+ }
--- stories/index.js
+++ stories/index.js
@@ -7,12 +7,14 @@ import "index.scss";
 
 import Button from "components/Button";
 
+
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
   .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Confirm", () => <Button ClassName = 'button--confirm' confirm>Confirm</Button>)
   .add("Danger", () => <Button danger>Cancel</Button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
